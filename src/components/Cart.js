335afb7a06183ecdd2a1ea0ded1cc75b2bd0d685
DarioHC <dariohcosta@gmail.com>
2023-03-03 16:31:45 -0300

    AC6: diseño Cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,20 +1,37 @@
 import { Box } from '@mui/material'
 import React, { useContext } from 'react'
 import { CartContext } from './context/CartContext'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemAvatar from '@mui/material/ListItemAvatar'
+import ListItemText from '@mui/material/ListItemText'
+import Avatar from '@mui/material/Avatar'
+import IconButton from '@mui/material/IconButton'
+import FolderIcon from '@mui/icons-material/Folder'
+import DeleteIcon from '@mui/icons-material/Delete'
 
 const Cart = () => {
   const { cart } = useContext(CartContext)
+
   return (
     <>
-      <Box pt={15}>
+      <Box sx={{ maxWidth: 'sm', boxShadow: 1 }} mt={15} ml={25}>
         {cart
           ? cart.map((curso) => (
-            <Box key={`${curso.id}`}>
-              <div>
-                <h3>{curso.nombre}</h3>
-                <h3>{curso.quantity}</h3>
-              </div>
-            </Box>
+            <List key={`${curso.id}`}>
+              <ListItem secondaryAction={
+                <IconButton edge='end' aria-label='delete'>
+                  <DeleteIcon />
+                </IconButton>
+              }
+              >
+                <ListItemAvatar>
+                  <img src={curso.imagen} width={80} alt={curso.nombre} />
+                </ListItemAvatar>
+                <ListItemText className='item-list'>{curso.nombre} x {curso.quantity}</ListItemText>
+                <ListItemText>${curso.precio}</ListItemText>
+              </ListItem>
+            </List>
           ))
           : 'loading...'}
       </Box>
