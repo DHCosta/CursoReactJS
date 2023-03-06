894dc83a130cc99d16618405936728b07e3bd6bc
DHCosta <dariohcosta@gmail.com>
2023-03-06 15:35:05 -0300

    AC6: parte de las funcionalidad de Cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,17 +1,21 @@
-import { Box } from '@mui/material'
+import { Box, Container, Typography } from '@mui/material'
 import React, { useContext } from 'react'
 import { CartContext } from './context/CartContext'
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import ListItemAvatar from '@mui/material/ListItemAvatar'
 import ListItemText from '@mui/material/ListItemText'
-import Avatar from '@mui/material/Avatar'
 import IconButton from '@mui/material/IconButton'
-import FolderIcon from '@mui/icons-material/Folder'
 import DeleteIcon from '@mui/icons-material/Delete'
+import Divider from '@mui/material/Divider'
+import Paper from '@mui/material/Paper'
 
 const Cart = () => {
-  const { cart } = useContext(CartContext)
+  const { cart, removeItem } = useContext(CartContext)
+
+  function calTotal () {
+    return cart.reduce((a, b) => a + (b.precio * b.quantity), 0)
+  }
 
   return (
     <>
@@ -20,7 +24,8 @@ const Cart = () => {
           ? cart.map((curso) => (
             <List key={`${curso.id}`}>
               <ListItem secondaryAction={
-                <IconButton edge='end' aria-label='delete'>
+                // eslint-disable-next-line no-restricted-globals
+                <IconButton edge='end' aria-label='delete' onClick={removeItem(event.target.id)}>
                   <DeleteIcon />
                 </IconButton>
               }
@@ -29,12 +34,16 @@ const Cart = () => {
                   <img src={curso.imagen} width={80} alt={curso.nombre} />
                 </ListItemAvatar>
                 <ListItemText className='item-list'>{curso.nombre} x {curso.quantity}</ListItemText>
-                <ListItemText>${curso.precio}</ListItemText>
+                <ListItemText>${curso.precio * curso.quantity}</ListItemText>
               </ListItem>
+              <Divider />
             </List>
           ))
           : 'loading...'}
       </Box>
+      {cart.length
+        ? <Paper sx={{ maxWidth: 'sm', elevation: 3, display: 'flex', justifyContent: 'flex-end', ml: 25, py: 3 }}><Typography sx={{ pr: 5 }}>TOTAL ${calTotal()}</Typography></Paper>
+        : 'No posee cursos agregados al Carrito'}
     </>
   )
 }
